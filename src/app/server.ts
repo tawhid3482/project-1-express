f1a04396ae3c5bfc3132068dcc0745de05d0d9a3
saikat <tawhidulislam3482@gmail.com>
2024-05-14 12:36:54 +0600

    http server

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,6 +1,9 @@
+import { Server } from 'http'
 import app from './app'
 
 const PORT = 5000
+
+let server : Server
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
