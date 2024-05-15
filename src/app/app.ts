ff7e6709525dafca59d2b191086aa7b925c50f4a
saikat <tawhidulislam3482@gmail.com>
2024-05-15 15:57:25 +0600

    server postman

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
-import express from "express"
+import express, { Request, Response, response } from "express"
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello developersss')
+})
+app.post("/",( req:Request , res: Response )=>{
+    console.log(req.body)
+    res.send('got data')
 })
 
 export default app
